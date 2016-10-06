afaa3b86fc911eabaa8f12b7521c14764d876f2c
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-10-06 11:55:34 +0200

    Count types/vals/defs

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "syntax-experiments"
 scalaVersion := "2.11.8"
 
 libraryDependencies += "org.scalameta" %% "scalameta" % "1.1.0"
+
+libraryDependencies += "org.apache.commons" % "commons-math3" % "3.6"
--- src/main/scala/syntax/Experiment.scala
+++ src/main/scala/syntax/Experiment.scala
@@ -7,10 +7,12 @@ import scala.meta.Type
 import scala.meta._
 import scala.reflect.ClassTag
 import scala.util.control.NonFatal
-
 import scala.collection.JavaConverters._
+
 import java.util.concurrent.CopyOnWriteArrayList
 import java.util.concurrent.atomic.AtomicInteger
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics
 
 object Experiment {
   def runAnalysis[T: ClassTag](
@@ -46,6 +48,19 @@ object Experiment {
     val compounds = runAnalysis[Type.Compound] {
       case t: Type.Compound => t
     }
+    val vals = new DescriptiveStatistics()
+    val defs = new DescriptiveStatistics()
+    val types = new DescriptiveStatistics()
+    compounds.foreach { x =>
+      x.refinement.foreach {
+        case _: Decl.Def => defs.addValue(1.0)
+        case _: Decl.Val => vals.addValue(1.0)
+        case _: Decl.Type => types.addValue(1.0)
+      }
+    }
     println(compounds.length)
+    println("vals: " + vals.getSum)
+    println("defs: " + defs.getSum)
+    println("types: " + types.getSum)
   }
 }
