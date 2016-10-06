bcad6c26625b91032a759904b86245d832d58258
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-10-06 11:35:04 +0200

    Infrastructure for experiments

--- src/main/scala/syntax/Experiment.scala
+++ src/main/scala/syntax/Experiment.scala
@@ -1,7 +1,51 @@
 package syntax
 
+import scala.collection.mutable
+import scala.meta.Source
+import scala.meta.Tree
+import scala.meta.Type
+import scala.meta._
+import scala.reflect.ClassTag
+import scala.util.control.NonFatal
+
+import scala.collection.JavaConverters._
+import java.util.concurrent.CopyOnWriteArrayList
+import java.util.concurrent.atomic.AtomicInteger
+
 object Experiment {
+  def runAnalysis[T: ClassTag](
+      analysis: PartialFunction[Tree, T]): mutable.Buffer[T] = {
+    val results = new CopyOnWriteArrayList[T]
+    val counter = new AtomicInteger()
+    ScalaFile.getAll.toVector.par.foreach { file =>
+      val n = counter.incrementAndGet()
+      if (n % 1000 == 0) {
+        println(n)
+      }
+      try {
+        file.read.parse[Source] match {
+          case Parsed.Success(ast) =>
+            ast.collect(analysis).foreach { t =>
+              results.add(t)
+            }
+          case _ =>
+        }
+      } catch {
+        case NonFatal(e) =>
+      }
+    }
+    results.asScala
+  }
+
+  type x = {
+    val y: Int
+  }
+
   def main(args: Array[String]): Unit = {
-    println("MAIN!")
+    println("Experiment!!!")
+    val compounds = runAnalysis[Type.Compound] {
+      case t: Type.Compound => t
+    }
+    println(compounds.length)
   }
 }
