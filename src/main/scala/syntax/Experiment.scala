5e61e4cea913e3e953d5279932ed48b015e2c005
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-12-12 10:44:47 +0100

    WIP

--- src/main/scala/syntax/Experiment.scala
+++ src/main/scala/syntax/Experiment.scala
@@ -1,18 +1,41 @@
 package syntax
 
+import scala.annotation.tailrec
+import scala.collection.JavaConverters._
 import scala.collection.mutable
 import scala.meta.Source
 import scala.meta.Tree
 import scala.meta.Type
+import scala.meta.Type.Compound
 import scala.meta._
 import scala.reflect.ClassTag
 import scala.util.control.NonFatal
-import scala.collection.JavaConverters._
 
 import java.util.concurrent.CopyOnWriteArrayList
 import java.util.concurrent.atomic.AtomicInteger
+trait Show[T] {
+  def print(e: T): String
+  def lineNumber(e: T): Int
+}
+object Show {
+  implicit def treeShow[T <: Tree]: Show[T] = new Show[T] {
+    override def print(e: T): String = e.syntax
+    override def lineNumber(e: T): Int = Experiment.linenumber(e)
+  }
+  def instance[T](f: T => String, g: T => Int): Show[T] = new Show[T] {
+    override def print(e: T): String = f(e)
+    override def lineNumber(e: T): Int = g(e)
+  }
+  implicit val compoundShow = new Show[Type.Compound] {
+    override def print(e: Type.Compound): String =
+      s"""
+         |```scala
+         |${e.syntax}
+         |```""".stripMargin
 
-import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics
+    override def lineNumber(e: Compound): Int = Experiment.linenumber(e)
+  }
+}
 
 object Experiment {
 
@@ -75,31 +98,97 @@ object Experiment {
     builder.result()
   }
 
+  def linenumber(t: Tree): Int =
+    t.tokens.headOption.map(_.pos.start.line + 1).getOrElse(0)
+
+  def prettyPrint[T](buf: Traversable[(ScalaFile, T)])(
+      implicit ev: Show[T]): String = {
+    buf.map {
+      case (file, t) =>
+        val url = file.githubUrlAtLine(ev.lineNumber(t))
+        s"$url: ${ev.print(t)}"
+    }.mkString("\n")
+  }
+
   def runTypeCompounds(): Unit = {
     val compounds = runAnalysis(30000)(collectAnalysis {
-      case t: Type.Compound =>
-        t.refinement
+      case t: Type.Compound
+          if t.refinement.exists(x => x.is[Decl.Var] || x.is[Defn.Var]) =>
+        t
     })
-    compounds.groupBy(_.getClass.getSimpleName).mapValues(_.length).foreach {
-      case (k, v) =>
-        println(s"$k: $v")
-    }
+    println("Total: " + compounds.length)
+    println(prettyPrint(compounds))
   }
 
   def runTypeProjections(): Unit = {
     val typProjections = runAnalysis(100000)(projectAnalysis)
-    typProjections.foreach {
-      case (file, t) =>
-        val url = file.githubUrlAtLine(
-          t.tokens.headOption.map(_.pos.start.line).getOrElse(0))
-        println(s"$url ${t.syntax}")
+    println("Total: " + typProjections.length)
+    println(prettyPrint(typProjections))
+  }
+
+  @tailrec
+  final def parents(tree: Tree,
+                    accum: Seq[Tree] = Seq.empty[Tree]): Seq[Tree] = {
+    tree.parent match {
+      case Some(parent) => parents(parent, parent +: accum)
+      case _ => accum
     }
-    println("typProjectsions: " + typProjections.length)
+  }
+
+  def runInfixNewline(): Unit = {
+
+    /**
+      * Creates lookup table from token offset to its closest scala.meta tree.
+      */
+    def getOwners(tree: Tree): Map[Token, Tree] = {
+      val result = Map.newBuilder[Token, Tree]
+      def loop(x: Tree): Unit = {
+        x.tokens.foreach { tok =>
+          result += tok -> x
+        }
+        x.children.foreach(loop)
+      }
+      loop(tree)
+      result.result()
+    }
+
+    def isSymbolic(nme: String) =
+      !nme.exists(x => Character.isLetterOrDigit(x))
+
+    val infixOwners = runAnalysis[InfixExperiment](1000) { t =>
+      val owners = getOwners(t)
+
+      def isCandidate(ft: FormatToken, tok: Token): Boolean =
+        ft.newlines > 0 &&
+          isSymbolic(tok.syntax) &&
+          !owners(tok).parent.exists(_.is[Pat])
+      val fts = FormatToken.formatTokens(t.tokens)
+      fts.collect {
+        // lines ending in infix operator
+        case ft @ FormatToken(tok: Token.Ident, _, _)
+            if isCandidate(ft, tok) =>
+          InfixExperiment(tok, owners(tok), startOfLine = false)
+        case ft @ FormatToken(_, tok: Token.Ident, _)
+            if isCandidate(ft, tok) =>
+          InfixExperiment(tok, owners(tok), startOfLine = true)
+      }
+    }
+//    infixOwners.groupBy(_._2.getClass.getName).mapValues(_.length).foreach {
+//      case (a, b) => println(s"$a: $b")
+//    }
+    val (start, end) = infixOwners.partition(_._2.startOfLine)
+    println(prettyPrint(start))
+    println("SOL: " + start.length)
+    println("SOL infix: " + start.count(_._2.ownerName.contains("ApplyInfix")))
+    println("EOL infix: " + end.count(_._2.ownerName.contains("ApplyInfix")))
+    println("Total: " + infixOwners.length)
   }
 
   def main(args: Array[String]): Unit = {
     println("Experiment!!!")
-    runTypeProjections()
+    runInfixNewline()
+//    runTypeProjections()
+//    runTypeCompounds()
   }
 }
 
@@ -117,6 +206,56 @@ object HasTypeStats {
   }
 
 }
+
+case class FormatToken(left: Token, right: Token, between: Vector[Token]) {
+
+  override def toString = s"${left.syntax}∙${right.syntax}"
+
+  def newlines: Int = between.count(_.is[Token.LF])
+
+  def inside(range: Set[Range]): Boolean = {
+    if (range.isEmpty) true
+    else range.exists(_.contains(right.pos.end.line))
+  }
+
+  val leftHasNewline: Boolean = left.syntax.contains('\n')
+}
+
+object FormatToken {
+
+  /**
+    * Convert scala.meta Tokens to FormatTokens.
+    *
+    * Since tokens might be very large, we try to allocate as
+    * little memory as possible.
+    */
+  def formatTokens(tokens: Tokens): Array[FormatToken] = {
+    var left = tokens.head
+    val result = Array.newBuilder[FormatToken]
+    val whitespace = Vector.newBuilder[Token]
+    tokens.toArray.foreach {
+      case t @ Whitespace() => whitespace += t
+      case right =>
+        val tok = FormatToken(left, right, whitespace.result)
+        result += tok
+        left = right
+        whitespace.clear()
+    }
+    result.result
+  }
+}
+
+object Whitespace {
+  def unapply(token: Token): Boolean = {
+    import Token._
+    token.is[Space] ||
+    token.is[Tab] ||
+    token.is[CR] ||
+    token.is[LF] ||
+    token.is[FF]
+  }
+}
+
 object HasTypeParams {
   def unapply(arg: Tree): Option[Seq[String]] = {
     val res = arg match {
@@ -127,5 +266,17 @@ object HasTypeParams {
     }
     res.map(_.map(_.name.syntax))
   }
+}
 
+case class InfixExperiment(tok: Token, owner: Tree, startOfLine: Boolean) {
+  def ownerName: String =
+    owner.parent.get.getClass.getName.stripPrefix("scala.meta.")
+}
+
+object InfixExperiment {
+  implicit val infixExperimentShow: Show[InfixExperiment] =
+    Show.instance[InfixExperiment](
+      x => s"${x.tok.syntax}: ${x.ownerName}",
+      x => Experiment.linenumber(x.owner)
+    )
 }
