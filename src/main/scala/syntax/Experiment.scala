2410acdee73630177ec3e32f35bbf2e6ef8bb8b3
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-06-20 16:05:43 +0200

    Skip infix apply.

--- src/main/scala/syntax/Experiment.scala
+++ src/main/scala/syntax/Experiment.scala
@@ -12,10 +12,10 @@ object Experiment {
     val results = SyntaxAnalysis.onParsed[Observation[Term.Name]](files) {
       source =>
         source.collect {
-          case Term.ApplyInfix(_, name, _, args) if args.length > 1 =>
-            Observation("infix call site with multiple args",
-                        name.pos.start.line,
-                        name)
+//          case Term.ApplyInfix(_, name, _, args) if args.length > 1 =>
+//            Observation("infix call site with multiple args",
+//                        name.pos.start.line,
+//                        name)
           case Defn.Def(_, name, _, Seq(params), _, _)
               if !name.value.forall(_.isLetterOrDigit) &&
                 params.length > 1 =>
@@ -26,6 +26,7 @@ object Experiment {
     }
     Observation.markdownTable(results)
   }
+
   def main(args: Array[String]): Unit = {
     val result = symbolicInfix(Corpus.fastparse)
     Files.write(Paths.get("target", "results.txt"), result.getBytes())
