3d8e72e6f99bb94b9703f43812dbf3ac8bf8ed37
Aggelos Biboudis <biboudis@gmail.com>
2017-08-31 14:54:36 +0200

    Add macro call-site, semantic experiments

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 name := "syntax-experiments"
 
-scalaVersion := "2.12.2"
+scalaVersion := "2.12.3"
 
-libraryDependencies += "org.scalameta" %% "testkit" % "1.8.0"
+libraryDependencies += "org.scalameta" %% "testkit" % "2.0.0-M3"
 
 scalacOptions += "-Yno-adapted-args"
--- src/main/scala/syntax/SyntacticExperiment.scala
+++ src/main/scala/syntax/SyntacticExperiment.scala
@@ -5,7 +5,7 @@ import java.nio.file.Paths
 import scala.meta._
 import testkit._
 
-object Experiment {
+object SyntacticExperiment {
 
   def symbolicInfix(corpus: Corpus): String = {
     val files = Corpus.files(corpus).toBuffer.par
@@ -14,17 +14,17 @@ object Experiment {
         source.collect {
           case Term.ApplyInfix(_, name, _, args) if args.length > 1 =>
             Observation(name.value,
-                        name.pos.start.line + 1,
+                        name.pos.startLine + 1,
                         s"infix call site with ${args.length} args")
           case Defn.Def(_, name, _, Seq(params), _, _)
               if !name.value.headOption.exists(x => x.isLetter || x == '_') &&
                 params.length > 1 =>
             Observation(name.value,
-                        name.pos.start.line + 1,
+                        name.pos.startLine + 1,
                         s"symbolic def with ${params.length} args")
         }
       }
-    Observation.markdownTable(results)
+    Observation.markdownTable(results.toList)
   }
 
   def main(args: Array[String]): Unit = {
